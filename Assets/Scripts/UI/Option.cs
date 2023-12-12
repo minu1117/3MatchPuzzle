07d79c19d2913ab86c77a18ba7b8fd12ee1761d3
함민우 <108320084+minu1117@users.noreply.github.com>
2023-12-12 21:30:25 +0900

    start scene의 옵션 ui 활성화/비활성화 방식 변경
    - 빈 오브젝트에 option 스크립트 연결, 항상 활성화 상태
    - 빈 오브젝트 하위에 실제 옵션 오브젝트 추가, 해당 오브젝트를 활성/비활성하는 방식으로 변경
    (게임 시작 전에 옵션 ui가 켜져있지 않아도 됨)

--- Assets/Scripts/UI/Option.cs
+++ Assets/Scripts/UI/Option.cs
@@ -3,18 +3,17 @@ using UnityEngine.UI;
 
 public class Option : MonoBehaviour
 {
+    [SerializeField] private GameObject optionObject;
     [SerializeField] private Button optionButton;
     [SerializeField] private Button exitButton;
     [SerializeField] private Slider bgmSlider;
     [SerializeField] private Slider sfxSlider;
-
+    
     public void ConnectButtonOnClick()
     {
-        optionButton.onClick.AddListener(() => gameObject.SetActive(true));
-        exitButton.onClick.AddListener(() => gameObject.SetActive(false));
+        optionButton.onClick.AddListener(() => optionObject.gameObject.SetActive(true));
+        exitButton.onClick.AddListener(() => optionObject.gameObject.SetActive(false));
         bgmSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeBGMVolume);
         sfxSlider.onValueChanged.AddListener(SoundManager.Instance.ChangeSFXVolume);
-
-        gameObject.SetActive(false);
     }
 }
